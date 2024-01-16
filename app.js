b281541810cae16decbf8accc13618ba56191b59
MavlonbekKhakijanov <khakijanovamor@gmail.com>
2024-01-16 15:35:19 +0300

    loyha yakunlandi

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
 const time = document.querySelector('#time');
-const hour = document.querySelector('#hour');
-const minute = document.querySelector('#minute');
-const seconde = document.querySelector('#seconde');
+const hourEl = document.querySelector('#hour');
+const minuteEl = document.querySelector('#minute');
+const secondeEl = document.querySelector('#seconde');
 const formCreat = document.querySelector('#form-creat');
 const formEdit = document.querySelector('#form-edit');
 const massage = document.querySelector('#message');
 const listGroup = document.querySelector('#list-group');
+const overlay = document.querySelector('#overlay');
+const closeEdit = document.querySelector('#close-edit')
 
 
-
-setInterval(snow, 100)
-function snow (){
-    const snows = document.createElement("i")
-    snows.classList.add('fas')
-    snows.classList.add('fa-snowflake')
-    snows.classList.add('snow')
-    
-
-    snows.style.left = Math.random() * window.innerWidth + "px"
-    snows.style.animationDuration = Math.random()* 3 + 2 + 's'
-    snows.style.fontSize = Math.random()* 7 + 12 + "px"
-    snows.style.opacity = Math.random()
-
-
-    document.body.append(snows);
-
-
-    setTimeout(()=>{
-        snows.remove()
-    }, 4000)
-
-}
-
-
-
-
+let EditeTodo 
 
 // local storageni tekshirish
-
-const todos = JSON.parse(localStorage.getItem('list'))
+let todos = JSON.parse(localStorage.getItem('list'))
 ? JSON.parse(localStorage.getItem('list'))
 : [];
 
@@ -54,9 +29,33 @@ function getTime(){
     const date = now.getDate()<10? '0'+ now.getDate():now.getDate()
     const hour = now.getHours()<10? '0'+  now.getHours(): now.getHours()
     const minute = now.getMinutes()<10 ? '0'+now.getMinutes():now.getMinutes()
-    const seconde = now.getSeconds()<10 ? '0'+now.getSeconds():now.getSeconds()   
+    const seconde = now.getSeconds()<10 ? '0'+now.getSeconds():now.getSeconds()  
+
+    const months = [
+        'Yanvar',
+        'Fevral',
+        'Mart',
+        'Aprel',
+        'May',
+        'Iyun',
+        'Iyul',
+        'Avgust',
+        'Sentabr',
+        'Oktabr',
+        'Noyabr',
+        'Dekabr',
+    ]
+    const month_title = now.getMonth()
+    time.textContent = `${date}-${months[month_title]} ${year}-yil`
+    hourEl.textContent = `${hour}:`
+    minuteEl.textContent=`${minute}:`
+    secondeEl.textContent = `${seconde}`
     return `${hour}:${minute}, ${date}.${month}.${year}`
+   
 }
+setInterval(()=>{
+    getTime()
+},1000)
 
 
 // functions
@@ -74,17 +73,61 @@ function showTodo(){
     todo.forEach((item, i)=>{
 
        listGroup.innerHTML += `
-       <li class="list-item">
+       <li ondblclick = (complated(${i})) class="list-item ${item.complated ? 'complated':''}">
          <p>${item.text}</p>
             <div class="icon-box">
              <small class="list-time">${item.time}</small>
-             <i class="fa-regular fa-pen-to-square edite"></i>
-             <i class="fa-regular fa-trash-can delete"></i>
+             <i onclick=(editTodo(${i})) class="fa-regular fa-pen-to-square edite"></i>
+             <i onclick=(deletTodo(${i})) class="fa-regular fa-trash-can delete"></i>
              </div>
         </li>
        `
     })
 }
+
+
+formEdit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputValue = formEdit['input-edit'].value.trim()
+    formEdit.reset()
+    if(inputValue.length){
+        todos.splice(EditeTodo, 1, 
+        {text:inputValue, time:getTime(), complated:false})
+        setTodo()
+        showTodo()
+        closse()
+    }else{
+        showMessage('#message', 'error')
+    }
+})
+// editTodo
+ function editTodo(id){
+    oppen()
+    EditeTodo = id
+ }
+// complated todo
+function complated(id){
+    const compaltedTodo = todos.map((item, i)=>{
+        if(id == i){
+            return {...item, complated:item.complated == true?false:true}
+        }else{
+            return{...item}
+        }
+    })
+    todos = compaltedTodo
+    setTodo();
+    showTodo();
+}
+// deletTodo function
+function deletTodo(id){
+    let deletedTodos = todos.filter((item, i)=>{
+        return id !== i
+    })
+    todos = deletedTodos
+    setTodo()
+    showTodo()
+}
+
 
 
 // show message
@@ -107,7 +150,66 @@ formCreat.addEventListener('submit', (e)=>{
     }else{
         showMessage('#message', 'error')
     }
+});
+
+function oppen(){
+    formEdit.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+
+}
+function closse(){
+    formEdit.classList.add('hidden');
+    overlay.classList.add('hidden')
+
+}
+overlay.addEventListener('click',()=>{
+    closse()
+})
+closeEdit.addEventListener('click',()=>{
+    closse()
 })
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setInterval(snow, 75)
+function snow (){
+    const snows = document.createElement("i")
+    snows.classList.add('fas')
+    snows.classList.add('fa-snowflake')
+    snows.classList.add('snow')
+    
+
+    snows.style.left = Math.random() * window.innerWidth + "px"
+    snows.style.animationDuration = Math.random()* 3 + 2 + 's'
+    snows.style.fontSize = Math.random()* 7 + 12 + "px"
+    snows.style.opacity = Math.random()
+
+
+    document.body.append(snows);
+
+
+    setTimeout(()=>{
+        snows.remove()
+    }, 4000)
+
+}
